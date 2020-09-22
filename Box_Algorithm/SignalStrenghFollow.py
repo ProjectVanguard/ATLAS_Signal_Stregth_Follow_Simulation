19ba5a4ab02dfcc40a75f70e484c9e7d40f97f8c
Portela1 <manuel.portela1@upr.edu>
2020-09-22 01:50:05 -0700

    Code finished and working. Needs some refactoring and documentation. More testing will be done to ensure is a viable option.

--- Box_Algorithm/SignalStrenghFollow.py
+++ Box_Algorithm/SignalStrenghFollow.py
@@ -1,85 +1,70 @@
-import random
 import math
 
-
-
-class Vector(object):
-
-    def __init__(self, x=0, y=0, z=0): self.x, self.y, self.z = x, y, z
-    def getx(self): return self.x
-    def gety(self): return self.y
-    def getz(self): return self.y
-    def pirntVector(self): print (self.x, " ", self.y, " ", self.z," ")
-
-
-
-class Pair(object):
-    def __init__(self, id, Vector:Vector,signal_strength): self.id, self.Vector, self.signal_strength = id, Vector, signal_strength
+class Data(object):
+    def __init__(self, id,x,y,z,signal_strength): self.id, self.signal_strength, self.x,self.y, self.z = id, signal_strength,x,y,z
 
     def getId(self): return self.id
-    def getV(self): return self.Vector
+    def getV(self): return [self.x,self.y,self.z]
     def getS(self): return self.signal_strength
-    def getfucked(self): return self.getV()
 
 class Drone(object):
     def __init__(self,x=0, y=0, z=0): self.x, self.y, self.z = x, y, z
+    def moveFr(self, x): self.x = float(self.x + x)
+    def moveRi(self, y): self.y = float(self.y + y) 
+    def moveUp(self, z): self.z = float(self.z + z) 
+    
+    
+    
 
-    def moveUp(self, z): self.x = float(self.z + z)
-    def moveDo(self, z): self.x = float(self.z - z)   
-    def moveRi(self, y): self.z = float(self.y + y) 
-    def moveLe(self, y): self.d = float(self.y - y)
-    def moveFr(self, x): self.d = float(self.x + x)
-    def moveBa(self, x): self.d = float(self.x - x)
-
-    def location(self):return Vector(self.x,self.y,self.z)
+    def location(self):return [self.x,self.y,self.z]
 
 def Calc_SS (Drone,Vector,INITIAL_DISTANCE):
     destination = Vector
     current_location = Drone.location()
-    current_distance = math.sqrt(((destination.getx()-current_location.getx())*(destination.getx()-current_location.getx())) +
-                     ((destination.gety()-current_location.gety())*(destination.gety()-current_location.gety())) + 
-                     ((destination.getz()-current_location.getz())*(destination.getz()-current_location.getz())))
+    current_distance = math.sqrt(((destination[0]-current_location[0])*(destination[0]-current_location[0])) +
+                     ((destination[1]-current_location[1])*(destination[1]-current_location[1])) + 
+                     ((destination[2]-current_location[2])*(destination[2]-current_location[2])))
     return ((INITIAL_DISTANCE-current_distance)/INITIAL_DISTANCE)*100
 
 def Box_mission(Drone,Vector,length_of_box,INITIAL_DISTANCE):
     box_plots = []
-    box_plots.append(Pair("T0",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    box_plots.append(Data("T0",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
     Drone.moveFr(length_of_box/2)
-    box_plots.append(Pair("T1",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
-    Drone.moveLe(length_of_box/2)
-    box_plots.append(Pair("T2",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
-    Drone.moveBa(length_of_box/2)
-    box_plots.append(Pair("T3",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
-    Drone.moveBa(length_of_box/2)
-    box_plots.append(Pair("T4",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    box_plots.append(Data("T1",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    Drone.moveRi((length_of_box/2) * -1)
+    box_plots.append(Data("T2",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    Drone.moveFr((length_of_box/2) * -1)
+    box_plots.append(Data("T3",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    Drone.moveFr((length_of_box/2) * -1)
+    box_plots.append(Data("T4",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
     Drone.moveRi(length_of_box/2)
-    box_plots.append(Pair("T5",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    box_plots.append(Data("T5",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
     Drone.moveRi(length_of_box/2)
-    box_plots.append(Pair("T6",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    box_plots.append(Data("T6",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
     Drone.moveFr(length_of_box/2)
-    box_plots.append(Pair("T7",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    box_plots.append(Data("T7",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
     Drone.moveFr(length_of_box/2)
-    box_plots.append(Pair("T8",Drone.location,Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
-    Drone.moveLe(length_of_box/2)
-    Drone.moveBa(length_of_box/2)
+    box_plots.append(Data("T8",Drone.location()[0],Drone.location()[1],Drone.location()[2],Calc_SS(Drone,Vector,INITIAL_DISTANCE)))
+    Drone.moveRi((length_of_box/2) * -1)
+    Drone.moveFr((length_of_box/2) * -1)
     return box_plots
 
-def move(Drone,Pair,Vector,INITIAL_DISTANCE):
+def move(Drone,Data,Vector,INITIAL_DISTANCE):
     t0 = Drone.location()
-    maxV = Pair.getV()
-    maxS = Pair.getS()
-    direction_of_travel = Vector(maxV.getx() - t0.getx(), maxV.gety() - t0.gety(), maxV.getz() - t0.getz())
-    Drone.moveFr(direction_of_travel.getx() * 2)
-    Drone.moveRi(direction_of_travel.gety() * 2)
-    Drone.moveUp(direction_of_travel.getz() * 2)
+    maxV = Data.getV()
+    maxS = Data.getS()
+    direction_of_travel = [maxV[0] - t0[0], maxV[1] - t0[1], maxV[2] - t0[2]]
+    Drone.moveFr(direction_of_travel[0] * 2)
+    Drone.moveRi(direction_of_travel[1] * 2)
+    Drone.moveUp(direction_of_travel[2] * 2)
     while(Calc_SS(Drone,Vector,INITIAL_DISTANCE) > maxS):
         maxS = Calc_SS(Drone,Vector,INITIAL_DISTANCE)
-        Drone.moveFr(direction_of_travel.getx())
-        Drone.moveRi(direction_of_travel.gety())
-        Drone.moveUp(direction_of_travel.getz())
-    Drone.moveBa(direction_of_travel.getx())
-    Drone.moveLe(direction_of_travel.gety())
-    Drone.moveDo(direction_of_travel.getz())
+        Drone.moveFr(direction_of_travel[0])
+        Drone.moveRi(direction_of_travel[1])
+        Drone.moveUp(direction_of_travel[2])
+    Drone.moveFr(direction_of_travel[0]*-1)
+    Drone.moveRi(direction_of_travel[1]*-1)
+    Drone.moveUp(direction_of_travel[2]*-1)
     
 
 
@@ -87,9 +72,9 @@ def move(Drone,Pair,Vector,INITIAL_DISTANCE):
 
 
 def Simulation(Drone,Vector,length_of_box):
-    INITIAL_DISTANCE = math.sqrt(((Vector.getx()-Drone.location().getx()*(Vector.getx()-Drone.location().getx())) +
-                     ((Vector.gety()-Drone.location().gety())*(Vector.gety()-Drone.location().gety())) + 
-                     ((Vector.getz()-Drone.location().getz())*(Vector.getz()-Drone.location().getz())))
+    INITIAL_DISTANCE = math.sqrt(((Vector[0]-Drone.location()[0]*(Vector[0]-Drone.location()[0])) +
+                     ((Vector[1]-Drone.location()[1])*(Vector[1]-Drone.location()[1])) + 
+                     ((Vector[2]-Drone.location()[2])*(Vector[2]-Drone.location()[2]))))
 
     temp_plot = Box_mission(Drone,Vector,length_of_box,INITIAL_DISTANCE)
     max = temp_plot[0]         
@@ -119,10 +104,9 @@ def Simulation(Drone,Vector,length_of_box):
     
 
 def main():
-    drone = Drone(50,50,300)
-    lizard = Vector(300,-800,300)
-    Simulation(drone,lizard,50)
-    
+    drone = Drone(0,0,100)
+    lizard = [1000,-500,100]
+    Simulation(drone,lizard,10)
    
 
 if __name__ == "__main__":
