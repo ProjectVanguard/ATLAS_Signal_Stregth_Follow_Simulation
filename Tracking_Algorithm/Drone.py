f41956ec4bf98e25206ad9ff2eef44b43e890d30
Portela1 <manuel.portela1@upr.edu>
2021-02-20 18:31:25 -0400

    drone movment and mode

--- Tracking_Algorithm/Drone.py
+++ Tracking_Algorithm/Drone.py
@@ -1,12 +1,15 @@
 
 class Drone:
-    def __init__(self, x=0, y=0, z=0):
+
+    def __init__(self, x=0, y=0, z=0, pointing_direction='', flight_mode=''):
         self.x = x
         self.y = y
         self.z = z
+        self.pointing_direction = pointing_direction
+        self.flight_mode = flight_mode
 
     def location(self):
-        return [self.x, self.y, self.z]
+        return [self.x, self.y, self.z, self.flight_mode, self.pointing_direction]
 
     def moveFr(self, x):
         self.x = float(self.x + x)
@@ -16,3 +19,12 @@ class Drone:
 
     def moveUp(self, z):
         self.z = float(self.z + z)
+
+    def point_towards(self, pointing_direction):
+        self.pointing_direction = pointing_direction
+
+    def get_mode(self):
+        return self.flight_mode
+
+    def set_mode(self, flight_mode):
+        self.flight_mode = flight_mode
